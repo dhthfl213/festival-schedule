a1d27f845cb922f19084be93e839f2ba78631399
github sohee <https://github.com/dhthfl213>
2024-10-31 18:22:06 +0900

    Updated the header and added search functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MainPage from './components/MainPage'; // MainPage 경로 수정
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <MainPage /> {/* MainPage 컴포넌트 추가 */}
     </div>
   );
 }
